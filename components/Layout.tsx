93ac81c9ae8bce3a0d448f32128b6eae76e68515
dustin-kav <sticksandstonescraftworks@gmail.com>
2022-12-15 15:19:51 -0500

    latest progress

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,5 @@
-import classNames from "classnames";
 import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useState, useMemo } from "react";
+import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const menuItems = [
@@ -12,86 +10,46 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     },
     {
       id: 2,
-      href: "/api/fhir/careTeam",
+      href: "/fhir/careTeam",
       title: "Care Team",
     },
     {
       id: 3,
-      href: "/api/fhir/conditions",
+      href: "/fhir/conditions",
       title: "Conditions",
     },
     {
       id: 4,
-      href: "/api/fhir/coverage",
+      href: "/fhir/coverage",
       title: "Cooverage",
     },
     {
       id: 5,
-      href: "/api/fhir/diagnosticReports",
+      href: "/fhir/diagnosticReports",
       title: "Diagnostic Reports",
     },
     {
       id: 6,
-      href: "/api/fhir/encounters",
+      href: "/fhir/encounters",
       title: "Encounters",
     },
     {
       id: 7,
-      href: "/api/fhir/explanationOfBenefits",
+      href: "/fhir/explanationOfBenefits",
       title: "Explanation Of Benefits",
     },
     {
       id: 8,
-      href: "/api/fhir/observations",
+      href: "/fhir/observations",
       title: "Observations",
     },
     {
       id: 9,
-      href: "/api/fhir/procedures",
+      href: "/fhir/procedures",
       title: "Procedures",
     },
   ];
-  const [toggleCollapse, setToggleCollapse] = useState(false);
-  const [isCollapsible, setIsCollapsible] = useState(false);
 
-  const router = useRouter();
-
-  const activeMenu = useMemo(
-    () => menuItems.find((menu) => menu.href === router.pathname),
-    [router.pathname]
-  );
-
-  const wrapperClasses = classNames(
-    "h-screen px-4 pt-8 pb-4 bg-light flex justify-between flex-col",
-    {
-      ["w-80"]: !toggleCollapse,
-      ["w-20"]: toggleCollapse,
-    }
-  );
-
-  const collapseIconClasses = classNames(
-    "p-4 rounded bg-light-lighter absolute right-0",
-    {
-      "rotate-180": toggleCollapse,
-    }
-  );
-
-  const getNavItemClasses = (menu: any) => {
-    return classNames(
-      "flex items-center cursor-pointer hover:bg-light-lighter rounded w-full overflow-hidden whitespace-nowrap",
-      {
-        ["bg-light-lighter"]: activeMenu?.id === menu.id,
-      }
-    );
-  };
-
-  const onMouseOver = () => {
-    setIsCollapsible(!isCollapsible);
-  };
-
-  const handleSidebarToggle = () => {
-    setToggleCollapse(!toggleCollapse);
-  };
   return (
     <div className="h-screen flex flex-row justify-start">
       <div className="bg-primary flex-1 p-4 text-white">
